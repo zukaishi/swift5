f266324e79ed8b5a7047333bcd44c8e51fe0885a
zukaishi <ishizuka215@gmail.com>
2020-05-06 22:41:57 +0900

    update

--- Swift5Button/Swift5Button/NextViewController.swift
+++ Swift5Button/Swift5Button/NextViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    @IBOutlet var changeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
+    @IBAction func change(_ sender: AnyObject) {
+        changeLabel.text = "暗号が解除されました"
+    }
+    
     /*
     // MARK: - Navigation
 
